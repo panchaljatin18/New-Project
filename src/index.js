c7b7406efbea0df4e45850c12294501a9382f3ae
Panchal Jatin <167307883+panchaljatin18@users.noreply.github.com>
2026-04-22 18:59:31 +0530

    updatedatabase

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+
+
+
+/*
 import express from "express"
 const app = express()
 
@@ -19,3 +32,4 @@ const app = express()
         throw err
     }
 })
+    */
